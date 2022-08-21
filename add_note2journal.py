b3d7b3a16aec10b91a1d163f6b8d0be6e86a579a
Roman Okatev <nebulawalker@live.ru>
2022-08-21 18:42:54 +0300

    Включен режим песочницы, рефакторинг, исправление бага.

--- add_note2journal.py
+++ add_note2journal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 from datetime import date, datetime, timedelta
 import argparse
 import json
@@ -8,13 +8,13 @@ from config import Settings
 
 
 WEEK_DAYS = {
-    1: u'понедельник',
-    2: u'вторник',
-    3: u'среда',
-    4: u'четверг',
-    5: u'пятница',
-    6: u'суббота',
-    7: u'воскресенье',
+    1: 'понедельник',
+    2: 'вторник',
+    3: 'среда',
+    4: 'четверг',
+    5: 'пятница',
+    6: 'суббота',
+    7: 'воскресенье',
 }
 
 
@@ -26,13 +26,15 @@ def is_valid_date(text):
 
         return datetime.strptime(text, "%Y-%m-%d").date()
     except ValueError:
-        msg = "Not a valid date: '{0}'.".format(text)
+        msg = f"Not a valid date: '{text}'"
         raise argparse.ArgumentTypeError(msg)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=u'Adds note to notebook "Дневник", uses template note')
-    parser.add_argument('date',
+    parser = argparse.ArgumentParser(
+        description='Adds note to notebook "Дневник", uses template note'
+    )
+    parser.add_argument('-date',
                         nargs='?',
                         type=is_valid_date,
                         help='date in format "YYYY-MM-DD"')
@@ -42,7 +44,7 @@ if __name__ == '__main__':
 
     client = EvernoteClient(
         token=config.EVERNOTE_PERSONAL_TOKEN,
-        sandbox=False # Default: True
+        sandbox=True  # Default: True
     )
     noteStore = client.get_note_store()
 
@@ -54,11 +56,14 @@ if __name__ == '__main__':
     print('Title Context is:')
     print(json.dumps(context, ensure_ascii=False, indent=4))
 
-    new_note = noteStore.copyNote(config.JOURNAL_TEMPLATE_NOTE_GUID, config.JOURNAL_NOTEBOOK_GUID)
-    utitle_without_comment = new_note.title.decode('utf8').split('#', 1)[0]
+    new_note = noteStore.copyNote(
+        config.JOURNAL_TEMPLATE_NOTE_GUID,
+        config.JOURNAL_NOTEBOOK_GUID
+    )
+    utitle_without_comment = new_note.title.split('#', 1)[0]
     utitle = utitle_without_comment.strip().format(**context)
-    new_note.title = utitle.encode('utf8')
+    new_note.title = utitle
     noteStore.updateNote(new_note)
-    
-    print(u'Note created: %s' % utitle)
+
+    print(f'Note created: {utitle}')
     print('Done')
